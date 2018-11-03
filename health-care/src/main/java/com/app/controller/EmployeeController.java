fdeec95f90b814f2142c2f75c5ad0d6e7dda974c
satya <kvsnarayana89@gmail.com>
2018-11-03 22:38:55 +0530

    save details

--- health-care/src/main/java/com/app/controller/EmployeeController.java
+++ health-care/src/main/java/com/app/controller/EmployeeController.java
@@ -16,90 +16,92 @@ import com.app.model.Employee;
 import com.app.service.IEmployeeService;
 import com.app.view.EmployeeExcelView;
 import com.app.view.EmployeePdfView;
+
 //ctrl+Shift+O(import)
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
 	@Autowired
 	private IEmployeeService service;
-	
-	//1. To Display Register Page
+
+	// 1. To Display Register Page
 	@RequestMapping("/register")
 	public String showRegPage(ModelMap map) {
-		//create ModelAttribute Object
-		map.addAttribute("employee",new Employee());
+		// create ModelAttribute Object
+		map.addAttribute("employee", new Employee());
 		return "EmployeeRegister";
 	}
-	//2. On Click Submit Button
-	@RequestMapping(value="/insert",method=RequestMethod.POST)
-	public String saveData(@ModelAttribute Employee employee,ModelMap map)
-	{
-		Integer empId=service.saveEmployee(employee);
-		String msg="Employee '"+empId+"' saved";
-		map.addAttribute("message",msg);
-		//clear from data
-		map.addAttribute("employee",new Employee());
+
+	// 2. On Click Submit Button
+	@RequestMapping(value = "/insert", method = RequestMethod.POST)
+	public String saveData(@ModelAttribute Employee employee, ModelMap map) {
+		Integer empId = service.saveEmployee(employee);
+		String msg = "Employee '" + empId + "' saved";
+		map.addAttribute("message", msg);
+		// clear from data
+		map.addAttribute("employee", new Employee());
 		return "EmployeeRegister";
 	}
-	//3.Get Data From DB To UI
+
+	// 3.Get Data From DB To UI
 	@RequestMapping("/all")
-	public String showData(ModelMap map)
-	{
-		List<Employee> emps=service.getAllEmployees();
-		map.addAttribute("list",emps);
+	public String showData(ModelMap map) {
+		List<Employee> emps = service.getAllEmployees();
+		map.addAttribute("list", emps);
 		return "EmployeeData";
 	}
-	//4.Delete Data in db
+
+	// 4.Delete Data in db
 	@RequestMapping("/delete")
-	public String deleteEmployee(@RequestParam("id")Integer empId,ModelMap map) {
-		
+	public String deleteEmployee(@RequestParam("id") Integer empId, ModelMap map) {
+
 		service.deleteEmployee(empId);
-		String msg="Employee '"+empId+"' Deleted";
-		List<Employee> emps=service.getAllEmployees();
-		//add ModeMap for controller to UI
-		map.addAttribute("message",msg);
-		map.addAttribute("list",emps);
+		String msg = "Employee '" + empId + "' Deleted";
+		List<Employee> emps = service.getAllEmployees();
+		// add ModeMap for controller to UI
+		map.addAttribute("message", msg);
+		map.addAttribute("list", emps);
 		return "EmployeeData";
 	}
-	
-	//5.show Edit Page
+
+	// 5.show Edit Page
 	@RequestMapping("/edit")
-	public String showEdit(@RequestParam("id")Integer empId,ModelMap map)
-	{
-		Employee emp=service.getOneEmployee(empId);
-		map.addAttribute("employee",emp);
+	public String showEdit(@RequestParam("id") Integer empId, ModelMap map) {
+		Employee emp = service.getOneEmployee(empId);
+		map.addAttribute("employee", emp);
 		return "EmployeeEdit";
 	}
-	//6. do UpdateOperation
-	@RequestMapping(value="/update",method=RequestMethod.POST)
-	public String updateEmployee(@ModelAttribute Employee employee,ModelMap map)
-	{
+
+	// 6. do UpdateOperation
+	@RequestMapping(value = "/update", method = RequestMethod.POST)
+	public String updateEmployee(@ModelAttribute Employee employee, ModelMap map) {
 		service.updateEmployee(employee);
-		String msg="Employee '"+employee.getEmpId()+"' Updated";
-		List<Employee> emps=service.getAllEmployees();
-		map.addAttribute("list",emps);
-		map.addAttribute("message",msg);
+		String msg = "Employee '" + employee.getEmpId() + "' Updated";
+		List<Employee> emps = service.getAllEmployees();
+		map.addAttribute("list", emps);
+		map.addAttribute("message", msg);
 		return "EmployeeData";
 	}
-	//7.export data to Excel file
+
+	// 7.export data to Excel file
 	@RequestMapping("/excel")
 	public ModelAndView showExcel() {
-		List<Employee> emps=service.getAllEmployees();
-		ModelAndView m=new ModelAndView();
+		List<Employee> emps = service.getAllEmployees();
+		ModelAndView m = new ModelAndView();
 		m.setView(new EmployeeExcelView());
-		m.addObject("emps",emps);
+		m.addObject("emps", emps);
 		return m;
 	}
-	//8.export data to pdf file
+
+	// 8.export data to pdf file
 	@RequestMapping("/pdf")
-	public ModelAndView showPdf()
-	{
-		List<Employee> emps=service.getAllEmployees();
-		ModelAndView m=new ModelAndView();
-		//set View Name
+	public ModelAndView showPdf() {
+		List<Employee> emps = service.getAllEmployees();
+		ModelAndView m = new ModelAndView();
+		// set View Name
 		m.setView(new EmployeePdfView());
-		//set model map
-		m.addObject("emps",emps);
+		// set model map
+		m.addObject("emps", emps);
 		return m;
 	}
 }
